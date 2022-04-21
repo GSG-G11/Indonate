0cd098a40bb94a04342cdcd3d3fefa085495b88f
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-21 18:51:36 +0300

    create category  module (database)

---
+++ server/database/modules/category.ts
@@ -0,0 +1,19 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/connection';
+
+const category = sequelize.define('categories', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  icon_url: {
+    type: DataTypes.STRING,
+  },
+});
+
+export default category;
