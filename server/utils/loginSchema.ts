876314ac41772d66caec48a02b16ed0f1254c14e
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-22 01:11:42 +0300

    edit login schema Relates #8

--- server/utils/loginSchema.ts
+++ server/utils/loginSchema.ts
@@ -1,11 +1,11 @@
 import * as Joi from 'joi';
 
 const loginSchema = Joi.object({
-  username: Joi.string().required().max(250).min(3),
   email: Joi.string()
-    .required()
     .max(250)
     .min(4)
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required(),
+  password: Joi.string().max(50).min(3).required(),
 });
 export default loginSchema;
