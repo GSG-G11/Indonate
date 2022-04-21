2e3d611b3313780b464f9dd05419d4034d2203a6
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-21 18:59:03 +0300

    create family  module (database) and change eslint roule

---
+++ server/database/modules/family.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/connection';
+
+const family = sequelize.define('families', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+});
+
+export default family;
---
+++ server/database/modules/index.ts
@@ -0,0 +1,11 @@
+import donor from './donor';
+import family from './family';
+import donation from './donation';
+import contact from './contact';
+import category from './category';
+import campaign from './campaign';
+import capon from './capon';
+
+export {
+  donor, family, donation, contact, category, campaign, capon,
+};
