83eb29a093dc824cf2e268aacc333872efc7f44e
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-22 16:26:34 +0300

    invoke fake data functions

---
+++ server/database/fakeData/index.ts
@@ -0,0 +1,15 @@
+import campaignsData from './campaignsData';
+import categoryData from './categoryData';
+import familyData from './familyData';
+import contactData from './contactData';
+import donorData from './donorData';
+import donationData from './donationData';
+import caponData from './caponData';
+
+campaignsData();
+categoryData();
+familyData();
+contactData();
+donorData();
+donationData();
+caponData();
