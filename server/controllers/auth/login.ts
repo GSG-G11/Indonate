f370c69542eaac5fd19005f8e774e70c254ba245
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-22 18:38:54 +0300

    Fix error handling with try-catch Relates #8

--- server/controllers/auth/login.ts
+++ server/controllers/auth/login.ts
@@ -10,41 +10,46 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
     await loginSchema.validateAsync({ email, password });
   } catch (error) {
-    next(new CustomedError(error.details[0].message, 200));
+    next(new CustomedError(error.details[0].message, 400));
   }
-  const user = await Donor.findOne({
-    where: {
-      email,
-    },
-  });
-  if (!user) {
-    next(
-      new CustomedError("Email doesn't exists, Try another one or sign up", 406),
+  try {
+    const user = await Donor.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw new CustomedError(
+        "Email doesn't exists, Try another one or sign up",
+        406,
+      );
+    }
+    const isPasswordValidate = await bcrypt.compare(
+      password,
+      user?.getDataValue('password'),
     );
+    if (!isPasswordValidate) {
+      throw new CustomedError('Incorrect password, please try again', 406);
+    }
+    const payload = {
+      id: user?.getDataValue('id'),
+      name: user?.getDataValue('name'),
+      isAdmin: user?.getDataValue('is_admin'),
+    };
+    const token = jwt.sign(payload, process.env.SECRET as string, {
+      expiresIn: '30d',
+      algorithm: 'HS256',
+    });
+    res
+      .status(200)
+      .cookie('ACCESS_TOKEN', token, {
+        maxAge: 900000,
+        httpOnly: true,
+      })
+      .json({ message: 'Successfully logged in', data: payload });
+  } catch (error) {
+    next(error);
   }
-  const isPasswordValidate = await bcrypt.compare(
-    password,
-    user?.getDataValue('password'),
-  );
-  if (!isPasswordValidate) {
-    next(new CustomedError('Incorrect password, please try again ', 406));
-  }
-  const payload = {
-    id: user?.getDataValue('id'),
-    name: user?.getDataValue('name'),
-    isAdmin: user?.getDataValue('is_admin'),
-  };
-  const token = jwt.sign(payload, process.env.SECRET as string, {
-    expiresIn: '30d',
-    algorithm: 'HS256',
-  });
-  res
-    .status(200)
-    .cookie('ACCESS_TOKEN', token, {
-      maxAge: 900000,
-      httpOnly: true,
-    })
-    .json({ message: 'Successfully logged in', data: payload });
 };
 
 export default login;
