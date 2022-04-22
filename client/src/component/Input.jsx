561db938f705f63dc94c8df756bc73fd7e6b2e57
HaniOlwan <heenoow@gmail.com>
2022-04-22 03:16:48 +0300

    validate login form email and password input Relates #15

--- client/src/component/Input.jsx
+++ client/src/component/Input.jsx
@@ -17,7 +17,7 @@ function FormInput({
 
   return (
     <div>
-      <Input className="emailInput" type={type} onChange={handleInputChange} placeholder={placeholder} value={input} />
+      <Input className="emailInput" type={type} onChange={handleInputChange} placeholder={placeholder} value={input} maxLength={type !== 'email' ? 12 : 64} />
     </div>
   );
 }
--- client/src/component/Signin.jsx
+++ client/src/component/Signin.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button } from 'antd';
 import 'antd/dist/antd.css';
 import './Signin.css';
 import FormInput from './Input';
@@ -6,20 +7,33 @@ import FormInput from './Input';
 function Signin() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [credentials, setCredentials] = useState({ email, password });
   const getEmailValue = (rawEmail) => {
     setEmail(rawEmail);
   };
 
   const getPasswordValue = (rawPassword) => {
-    console.log(rawPassword);
     setPassword(rawPassword);
   };
 
+  const validateCredentials = () => {
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!email.match(validRegex)) {
+      alert('Invalid Email');
+    } else if (password.length < 6) {
+      alert('Password is short');
+    } else {
+      setCredentials({ email, password });
+      alert(credentials);
+    }
+  };
+
   return (
+
     <>
-      <FormInput type="email" placeholder="Email" getInputValue={getEmailValue} emailValue={email} />
-      <FormInput type="password" placeholder="Password" getInputValue={getPasswordValue} passwordValue={password} />
+      <FormInput type="email" placeholder="Email" getInputValue={getEmailValue} />
+      <FormInput type="password" placeholder="Password" getInputValue={getPasswordValue} />
+      <Button type="success" onClick={validateCredentials}>Login</Button>
     </>
   );
 }
