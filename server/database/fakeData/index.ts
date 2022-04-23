fe760baa1b2cf04fb2f8028c80c9b8bb5fd67050
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-23 04:56:16 +0300

    Delete all files and replace them with a data file

--- server/database/fakeData/index.ts
+++ server/database/fakeData/index.ts
@@ -1,12 +1,6 @@
 import sequelize from '../config/connection';
 // eslint-disable-next-line import/extensions
-import * as allCampaignModule from './campaignData.json';
-import * as caponModule from './caponData.json';
-import * as familyModule from './familyData.json';
-import * as contactModule from './contactData.json';
-import * as donorModule from './donorData.json';
-import * as donationModule from './donationData.json';
-import * as categoryModule from './categoryData.json';
+import * as data from './data.json';
 
 import {
   Campaign,
@@ -18,38 +12,34 @@ import {
   Capon,
 } from '../models';
 
-const { data: allCampaign } = allCampaignModule;
-const { data: allCapon } = caponModule;
-const { data: allFamilies } = familyModule;
-const { data: categories } = categoryModule;
-const { data: allMessages } = contactModule;
-const { data: allDonors } = donorModule;
-const { data: allDonation } = donationModule;
+const {
+  campaigns, capons, categories, messages, donations, donors, families,
+} = data;
 
 const buildData = async () => {
   await sequelize.sync({ force: true });
-  // };
+
   if (process.env.NODE_ENV === 'dev') {
     await Promise.all([
-      allCampaign.map(async (campaign: any) => {
+      campaigns.map(async (campaign: any) => {
         await Campaign.create(campaign);
       }),
       categories.map(async (category: any) => {
         await Category.create(category);
       }),
-      allMessages.map(async (message: any) => {
+      messages.map(async (message: any) => {
         await Contact.create(message);
       }),
-      allFamilies.map(async (family: any) => {
+      families.map(async (family: any) => {
         await Family.create(family);
       }),
-      ...allDonors.map(async (donor: any) => {
+      donors.map(async (donor: any) => {
         await Donor.create(donor);
       }),
-      ...allDonation.map(async (donation: any) => {
+      donations.map(async (donation: any) => {
         await Donation.create(donation);
       }),
-      allCapon.map(async (capon: any) => {
+      capons.map(async (capon: any) => {
         await Capon.create(capon);
       }),
     ]);
