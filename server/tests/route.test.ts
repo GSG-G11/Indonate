72e6f7f1a202f84c5371d6e96f98ae93fe5749fd
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-23 21:03:57 +0300

    Create unit tests for POST '/api/login' Relates #8

--- server/tests/route.test.ts
+++ server/tests/route.test.ts
@@ -1,18 +1,122 @@
+/* eslint-disable no-prototype-builtins */
 /* eslint-disable no-undef */
 import request from 'supertest';
 import connection from '../database/config/connection';
 import app from '../app';
+import buildFakeData from '../database/fakeData/buildFakeData';
 
-console.log(process.env.TEST_DB_URL);
-afterAll(() => {
-  connection.close();
-});
+beforeAll(() => buildFakeData());
 
 describe('POST/login', () => {
-  test('', () => {
-    request(app).post('/login').send({
-      email: 'mohammed@gmail.com',
-      password: '$2a$10$zak/42j/GpDReRtzYC/iEef.ZR6HRJPNy0yvUxn54632Ekhr9Q7se',
-    });
+  test('User with admin role', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'admin@gmail.com',
+        password: '123456789',
+      })
+      .expect(200);
+    expect(response.body.data.isAdmin).toEqual(true);
+    expect(response.body.data.name).toEqual('admin');
+    expect(response.body.data.hasOwnProperty('id')).toEqual(true);
+    expect(response.body.message).toBe('Successfully logged in');
   });
+
+  test('User without admin role (Normal User)', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'hosam@gmail.com',
+        password: '123456789',
+      })
+      .expect(200);
+    expect(response.body.data.hasOwnProperty('id')).toEqual(true);
+    expect(response.body.data.isAdmin).toEqual(false);
+    expect(response.body.message).toBe('Successfully logged in');
+  });
+
+  test('If is there access token', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'admin@gmail.com',
+        password: '123456789',
+      })
+      .expect(200);
+    expect(
+      response.headers['set-cookie'][0].startsWith('ACCESS_TOKEN'),
+    ).toEqual(true);
+  });
+
+  test('User with not valid email', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'hosam@gmail.co',
+        password: '12345678',
+      })
+      .expect(400);
+    expect(response.body.message).toBe('"email" must be a valid email');
+  });
+
+  test('User with not valid password', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'hosam@gmail.com',
+        password: '12',
+      })
+      .expect(400);
+    expect(response.body.message).toBe(
+      '"password" length must be at least 3 characters long',
+    );
+  });
+
+  test('Required email', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        password: '123456789',
+      })
+      .expect(400);
+    expect(response.body.message).toBe('"email" is required');
+  });
+
+  test('Required password', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'hosam@gmail.com',
+      })
+      .expect(400);
+    expect(response.body.message).toBe('"password" is required');
+  });
+
+  test('User with incorrect password', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'hosam@gmail.com',
+        password: '123456',
+      })
+      .expect(400);
+    expect(response.body.message).toBe('Incorrect password, please try again');
+  });
+
+  test('User with incorrect email', async () => {
+    const response = await request(app)
+      .post('/api/login')
+      .send({
+        email: 'hosa@gmail.com',
+        password: '123456789',
+      })
+      .expect(400);
+    expect(response.body.message).toBe(
+      "Email doesn't exists, Try another one or sign up",
+    );
+  });
+});
+
+afterAll(() => {
+  connection.close();
 });
