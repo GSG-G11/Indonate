9dc2b09dcd571c73c5d3ec0b03815dc35ac046da
farah2003 <farah.shaqoura@example.com>
2022-04-23 03:49:34 +0300

    make styles for signup page

--- client/src/Component/signUp/index.jsx
+++ client/src/Component/signUp/index.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 // import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import {
-  Form, Button, Input, message,
+  Form, Button, Input, message, Space, Typography,
 } from 'antd';
 import { signUp } from '../../feature/userReducer';
 import './style.css';
@@ -32,100 +32,127 @@ function SignUp() {
   };
 
   return (
-    <div>
-      <Form
-        name="register"
-        onFinish={Registor}
-      >
-        <Form.Item
-          name="name"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your nickname!',
-              whitespace: true,
-            },
-          ]}
+    <div className="sign-up-container">
+      <div className="img-side-sgin-up">
+        <Typography.Title level={3} style={{ margin: 0 }}>
+          Subsicribe with us to make yourself a contributor to charity and help people in need.
+          Subsicribe with us to make yourself a contributor to charity and help people in need.
+        </Typography.Title>
+      </div>
+      <div className="form-conatainer-signup">
+        <Space
+          direction="vertical"
+          style={{
+            height: '100%',
+            justifyContent: 'center',
+            width: '100%',
+            alignItems: 'center',
+          }}
         >
-          <Input
-            placeholder="Name"
-            onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
-          />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
-            },
-            {
-              required: true,
-              message: 'Please input your E-mail!',
-            },
-          ]}
-        >
-          <Input
-            placeholder="Email"
-            onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
-          />
-        </Form.Item>
-        <Form.Item
-          name="phone"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your phone number!',
-            },
-          ]}
-        >
-          <Input
+          <Typography.Title
+            level={3}
+          >
+            Register
+          </Typography.Title>
+          <Form
             style={{
-              width: '100%',
+              width: '30vw',
             }}
-            placeholder="Phone Number"
-            onChange={(e) => setUserInfo({ ...userInfo, phone: e.target.value })}
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder="Password"
-            onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
-          />
-        </Form.Item>
-        <Form.Item
-          name="confirm"
-          dependencies={['password']}
-          rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
+            name="register"
+            onFinish={Registor}
+          >
+            <Form.Item
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your nickname!',
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input
+                placeholder="Name"
+                onChange={(e) => setUserInfo({ ...userInfo, name: e.target.value })}
+              />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
+              <Input
+                placeholder="Email"
+                onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
+              />
+            </Form.Item>
+            <Form.Item
+              name="phone"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your phone number!',
+                },
+              ]}
+            >
+              <Input
+                style={{
+                  width: '100%',
+                }}
+                placeholder="Phone Number"
+                onChange={(e) => setUserInfo({ ...userInfo, phone: e.target.value })}
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password
+                placeholder="Password"
+                onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
+              />
+            </Form.Item>
+            <Form.Item
+              name="confirm"
+              dependencies={['password']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
 
-                return Promise.reject(new Error('The two passwords that you entered do not match!'));
-              },
-            }),
-          ]}
-        >
-          <Input.Password placeholder="Confirm password" />
-        </Form.Item>
-        <Button type="primary" htmlType="submit">
-          Sign Up
-        </Button>
-      </Form>
+                    return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                  },
+                }),
+              ]}
+            >
+              <Input.Password placeholder="Confirm password" />
+            </Form.Item>
+            <Button className="sign-up-btn" type="none" htmlType="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Space>
+
+      </div>
     </div>
   );
 }
--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,7 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const controllers_1 = __importDefault(require("../controllers"));
 const router = express_1.default.Router();
-router.post('/signup', controllers_1.default);
 exports.default = router;
