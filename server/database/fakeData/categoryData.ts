3cb2e068a43ceeb13d4ac4994d454c4e11928f50
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-23 04:20:30 +0300

    remove the function inside the catgeory data file

--- server/database/fakeData/categoryData.ts
+++ server/database/fakeData/categoryData.ts
@@ -1,25 +1,20 @@
-import { Category } from '../models/index';
-
-const allCategories = [
+const categories = [
   {
     name: 'Food',
+    icon_url: 'https://i.pinimg.com/564x/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.jpg',
   },
   {
     name: 'Money',
+    icon_url: 'https://i.pinimg.com/564x/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.jpg',
   },
   {
     name: 'Clothes',
+    icon_url: 'https://i.pinimg.com/564x/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.jpg',
   },
   {
     name: 'Education',
+    icon_url: 'https://i.pinimg.com/564x/dd/9d/c9/dd9dc9d83423bc037b511d73b29e6b80.jpg',
   },
 ];
-const categoryData = async () => {
-  await Promise.all(
-    allCategories.map(async (category) => {
-      await Category.create(category);
-    }),
-  );
-};
 
-export default categoryData;
+export default categories;
