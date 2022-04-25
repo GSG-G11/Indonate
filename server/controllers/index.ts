3987350a84d451e85b1efe05415cb1a0388973c4
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-25 15:02:50 +0300

    create get/campain/:id

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,5 +1,11 @@
 import signUp from './auth/signUp';
 import login from './auth/login';
 import logout from './auth/logout';
+import getCampaignById from './user/getCampaignById';
 
-export { login, signUp, logout };
+export {
+  login,
+  signUp,
+  logout,
+  getCampaignById,
+};
---
+++ server/controllers/user/getCampaignById.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { Campaign } from '../../database/models';
+import { CustomedError } from '../../utils';
+import { paramsSchema } from '../../utils/validation';
+
+const getCampaignById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await paramsSchema.validateAsync(req.params);
+    const { id } = result;
+    const campaignInfo = await Campaign.findByPk(id);
+    if (!campaignInfo) throw new CustomedError('there is no campaign', 400);
+    res.status(200).json(campaignInfo);
+  } catch (error) {
+    if (error.name === 'ValidationError') next(new CustomedError(error.details[0].message, 400));
+    next(error);
+  }
+};
+
+export default getCampaignById;
