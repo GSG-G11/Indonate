e586fcd6e42c5f1336b3583d0e695096a782f7d6
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-25 23:03:01 +0300

    Edit response data

--- server/controllers/categories/getAllCategories.ts
+++ server/controllers/categories/getAllCategories.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Response, Request } from 'express';
 import { Category } from '../../database/models';
-import { CustomedError } from '../../utils';
 
 const getAllCategories = async (
   req: Request,
@@ -12,7 +11,6 @@ const getAllCategories = async (
       attributes: ['id', 'name', 'icon_url'],
       raw: true,
     });
-    if (categories.length === 0) throw new CustomedError('There is no categories', 400);
     res.json({ message: 'Success', data: { categories } });
   } catch (error) {
     next(error);
