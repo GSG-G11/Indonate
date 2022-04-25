688a5c64c934c2c4b07eafbf89bd082f6ff97ccf
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-25 16:54:56 +0300

    create reports controller

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,5 +1,11 @@
 import signUp from './auth/signUp';
 import login from './auth/login';
 import logout from './auth/logout';
+import reports from './user/reports';
 
-export { login, signUp, logout };
+export {
+  login,
+  signUp,
+  logout,
+  reports,
+};
