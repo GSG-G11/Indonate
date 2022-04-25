0af017f4a05b494367179d2ec18ece70cb4bfa9d
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-25 14:58:42 +0300

    create params schema

--- server/utils/validation/index.ts
+++ server/utils/validation/index.ts
@@ -1,4 +1,5 @@
 import signUpSchema from './signUpSchema';
 import loginSchema from './loginSchema';
+import paramsSchema from './paramsSchema';
 
-export { loginSchema, signUpSchema };
+export { loginSchema, signUpSchema, paramsSchema };
