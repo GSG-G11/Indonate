e656345788451111fd552007c270b38037b2f2ac
HaniOlwan <heenoow@gmail.com>
2022-04-27 02:40:07 +0300

    edit card styles title button and cover Relates #51

--- client/src/component/campaign.jsx
+++ client/src/component/campaign.jsx
@@ -1,18 +1,55 @@
 import React from 'react';
-import { Card } from 'antd';
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import {
+  Card, Button, Typography,
+} from 'antd';
+import styles from './campaign.less';
+import 'antd/dist/antd.less';
+
+const { Text } = Typography;
 
 const { Meta } = Card;
 
 function Campaign() {
   return (
-    <Card
-      hoverable
-      style={{ width: 240 }}
-      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-    >
-      <Meta title="Europe Street beat" description="www.instagram.com" />
-    </Card>
+    <div className="center">
+      <Card
+        className="customCard"
+        hoverable
+        cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
+      >
+        <Meta className={styles.title} title="Give african child a good educations Give african child a good educations" />
+        <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
+
+        <Button type="primary">
+          Donate
+        </Button>
+      </Card>
+      <Card
+        className="customCard"
+        hoverable
+        cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
+      >
+        <Meta className={styles.title} title="Give african child a good educations" />
+        <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
+
+        <Button type="primary">
+          Donate
+        </Button>
+      </Card>
+      <Card
+        className="customCard"
+        hoverable
+        cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
+      >
+        <Meta className={styles.title} title="Give african child a good educations Give african child a good educations" />
+        <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
+
+        <Button type="primary">
+          Donate
+        </Button>
+      </Card>
+
+    </div>
   );
 }
 
