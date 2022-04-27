401750eef2832e12a7d8bfc7e765ecd48f622d30
farah2003 <farah.shaqoura@example.com>
2022-04-27 12:11:14 +0300

    try Segmented component

--- client/src/component/Campaigns.js
+++ client/src/component/Campaigns.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
-import { message } from 'antd';
+import { message, Segmented } from 'antd';
 
 function Compaigns() {
   useEffect(() => {
@@ -16,7 +16,7 @@ function Compaigns() {
   }, []);
   return (
     <div>
-      h1
+      <Segmented block options={[123, 456, 'longtext-longtext-longtext-longtext']} />
     </div>
   );
 }
