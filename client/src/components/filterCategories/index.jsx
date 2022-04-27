949ddc59adaa07be07d73f4a6699c81219347496
farah2003 <farah.shaqoura@example.com>
2022-04-28 00:52:19 +0300

    add search and avilable

--- client/src/components/filterCategories/index.jsx
+++ client/src/components/filterCategories/index.jsx
@@ -1,10 +1,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import {
-  message, Radio,
+  message,
+  Radio,
+  Input,
+  Segmented,
+
 } from 'antd';
 import './style.less';
 
+// import { CloseOutlined, CheckOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
 function Compaigns() {
   const [categories, setCategories] = useState([]);
 
@@ -12,7 +19,7 @@ function Compaigns() {
     const fetchData = async () => {
       try {
         const { data: { data: { categories: categoriesFromDB } } } = await axios('/api/categories');
-        const categoriesNameArr = ['List All'];
+        const categoriesNameArr = ['List All', 'Health2'];
         categoriesFromDB.map((item) => categoriesNameArr.push(item.name));
         setCategories(categoriesNameArr);
       } catch ({ response: { data: { message: errorMessage } } }) {
@@ -25,9 +32,31 @@ function Compaigns() {
 
   };
   return (
-    <Radio.Group onChange={onChange} defaultValue="List All" buttonStyle="solid">
-      {categories.map((item) => <Radio.Button value={item}>{item}</Radio.Button>)}
-    </Radio.Group>
+    <div className="fliter-section">
+      <div className="category">
+        <Radio.Group onChange={onChange} defaultValue="List All" buttonStyle="solid">
+          {categories.map((item) => <Radio.Button value={item}>{item}</Radio.Button>)}
+        </Radio.Group>
+      </div>
+      {/* <div>
+        Available campaigns:
+        {' '}
+        <Switch
+          checkedChildren={<CheckOutlined />}
+          unCheckedChildren={<CloseOutlined />}
+          defaultChecked
+        />
+      </div> */}
+      {/* <Radio.Group onChange={onChange} defaultValue="avilable" buttonStyle="solid">
+        <Radio.Button value="avilable">
+          Avilable
+        </Radio.Button>
+        <Radio.Button value="notavilable">Not avilable</Radio.Button>
+    </Radio.Group> */}
+
+      <Segmented options={['avilable', 'Not avilable']} />
+      <Search placeholder="input search text" className="search" style={{ width: '300px' }} />
+    </div>
   );
 }
 
