9178d855c5086e020783fdae97c64960f7c31086
HaniOlwan <heenoow@gmail.com>
2022-04-27 03:06:50 +0300

    cover image
    add top border radius
    Relates #51

--- client/src/component/campaign.jsx
+++ client/src/component/campaign.jsx
@@ -6,51 +6,22 @@ import styles from './campaign.less';
 import 'antd/dist/antd.less';
 
 const { Text } = Typography;
-
 const { Meta } = Card;
 
 function Campaign() {
   return (
-    <div className="center">
-      <Card
-        className="customCard"
-        hoverable
-        cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
-      >
-        <img className="category" src="https://i.ibb.co/W2Pm8G5/image-3.png" alt="Category" />
-        <Meta className={styles.title} title="Give african child a good educations Give african child a good educations" />
-        <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
-        <Button type="primary">
-          Donate
-        </Button>
-      </Card>
-      <Card
-        className="customCard"
-        hoverable
-        cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
-      >
-        <img className="category" src="https://i.ibb.co/W2Pm8G5/image-3.png" alt="Category" />
-        <Meta className={styles.title} title="Give african child a good educations" />
-        <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
-        <Button type="primary">
-          Donate
-        </Button>
-      </Card>
-      <Card
-        className="customCard"
-        hoverable
-        cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
-      >
-        <img className="category" src="https://i.ibb.co/W2Pm8G5/image-3.png" alt="Category" />
-        <Meta className={styles.title} title="Give african child a good educations Give african child a good educations" />
-        <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
-
-        <Button type="primary">
-          Donate
-        </Button>
-      </Card>
-
-    </div>
+    <Card
+      className="customCard"
+      hoverable
+      cover={<img alt="card cover" src="https://i.ibb.co/Dftbkqx/135619740-1012632809232481-290318453708399629-n.jpg" />}
+    >
+      <img className="category" src="https://i.ibb.co/W2Pm8G5/image-3.png" alt="Category" />
+      <Meta className={styles.title} title="Give african child a good educations Give african child a good educations" />
+      <Text type="secondary">Aellentesque porttitor lacus quis enim varius sed efficitur...</Text>
+      <Button type="primary">
+        Donate
+      </Button>
+    </Card>
   );
 }
 
