805884e81da663dfc51a1201eba341327315d375
farah2003 <farah.shaqoura@example.com>
2022-04-27 22:01:50 +0300

    add auth user middleware

--- server/controllers/auth/checkAuth.ts
+++ server/controllers/auth/checkAuth.ts
@@ -1,12 +1,6 @@
 import { Response } from 'express';
 
 const checkUser = (req:any, res:Response) => {
-  res.status(200).json({
-    data: [{
-      id: req.user.id,
-      name: req.user.name,
-      isAdmin: req.user.isAdmin,
-    }],
-  });
+  res.status(200).json({ data: { user: req.user } });
 };
 export default checkUser;
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import {
   signUp, login, logout, checkUser,
 } from '../controllers';
+import { authUser } from '../middlewares';
 
 const authRouter = Router();
 
 authRouter.post('/login', login);
-authRouter.get('/checkAuth', checkUser);
+authRouter.get('/checkAuth', authUser, checkUser);
 authRouter.post('/signUp', signUp);
 authRouter.post('/logout', logout);
 
