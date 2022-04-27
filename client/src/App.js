f9c9752f5d9bea05ec4c19ca9bfeba691ddf18b2
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-04-27 05:22:20 +0300

    remove div from app

--- client/src/App.js
+++ client/src/App.js
@@ -9,49 +9,47 @@ function App() {
     store.dispatch(getUserData());
   }, []);
   return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <h1>Home</h1>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <h1>Home</h1>
             }
-          />
-          <Route
-            path="/about-us"
-            element={
-              <h1>about-us</h1>
+        />
+        <Route
+          path="/about-us"
+          element={
+            <h1>about-us</h1>
             }
-          />
-          <Route
-            path="/campaigns"
-            element={
-              <h1>campaigns</h1>
+        />
+        <Route
+          path="/campaigns"
+          element={
+            <h1>campaigns</h1>
             }
-          />
-          <Route
-            path="/contact-us"
-            element={
-              <h1>contact-us</h1>
+        />
+        <Route
+          path="/contact-us"
+          element={
+            <h1>contact-us</h1>
             }
-          />
-          <Route
-            path="/signUp"
-            element={
-              <h1>signUp</h1>
+        />
+        <Route
+          path="/signUp"
+          element={
+            <h1>signUp</h1>
             }
-          />
-          <Route
-            path="/login"
-            element={
-              <h1>login</h1>
+        />
+        <Route
+          path="/login"
+          element={
+            <h1>login</h1>
             }
-          />
-        </Routes>
-      </Router>
-    </div>
+        />
+      </Routes>
+    </Router>
   );
 }
 
