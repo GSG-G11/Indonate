6f667691209a159fa788e670457f679c2d4bca7e
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-27 13:46:06 +0300

    Create Our Mission component

---
+++ client/src/components/OurMission/OurMission.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './OurMission.css';
+
+function OurMission() {
+  return (
+    <section className="our-mission-section">
+      <div className="our-mission-image-container">
+        <div className="green-background" />
+      </div>
+      <div className="our-mission-description-container">
+        <p className="our-mission-description-title">Our Mission</p>
+        <p className="our-mission-description-paragraph">
+          We At Our Company name, we give the best services for users and for
+          caregivers to donate their extra money so that we steal the money from
+          them.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default OurMission;
