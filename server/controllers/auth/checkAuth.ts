4c37b8afc8812ff9df4095a069d1f7c019c6ce08
farah2003 <farah.shaqoura@example.com>
2022-04-27 22:08:11 +0300

    remove status

--- server/controllers/auth/checkAuth.ts
+++ server/controllers/auth/checkAuth.ts
@@ -1,6 +1,6 @@
 import { Response } from 'express';
 
 const checkUser = (req:any, res:Response) => {
-  res.status(200).json({ data: { user: req.user } });
+  res.json({ data: { user: req.user } });
 };
 export default checkUser;
