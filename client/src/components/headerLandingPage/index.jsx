f41883c652c7be90568c21e07fa75af1d71cdd47
farah2003 <farah.shaqoura@example.com>
2022-04-30 02:26:12 +0300

    create fetch function

--- client/src/components/headerLandingPage/index.jsx
+++ client/src/components/headerLandingPage/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Typography } from 'antd';
 import './style.less';
 
 const { Text } = Typography;
 function HeaderLandingPage() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const { CancelToken: { source } } = axios;
+    const { token, cancel } = source;
+    source();
+    const fetchData = async () => {
+      const { data: { data: dataFromDB } } = await axios.get('/api/statistics', { cancelToken: token });
+      setData(dataFromDB);
+    };
+    fetchData();
+    return () => cancel();
+  }, []);
   return (
 
     <div className="Header-section">
@@ -17,7 +30,8 @@ function HeaderLandingPage() {
         </Text>
       </div>
       <div className="statistic-section">
-        h
+        <h1>{data.families}</h1>
+
       </div>
     </div>
 
