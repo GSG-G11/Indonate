da70e4c20f891c9d708bf81a8daf7cba0d8e6d96
HaniOlwan <heenoow@gmail.com>
2022-04-29 18:04:25 +0300

    add fake data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
-  Nav, SignUp, Signin, Review,
+  Nav, SignUp, Signin, Review, LatestCampaigns,
 } from './components';
 import store from './redux/app/store';
 import { getUserData } from './redux/feature/user/userSlice';
@@ -10,9 +10,29 @@ function App() {
   useEffect(() => {
     store.dispatch(getUserData());
   }, []);
+  const campaigns = [
+    {
+      title: 'Give African children a Good Education',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec vitae bibendum dolor, at finibus ',
+      imgSrc: 'https://i.ibb.co/41n9Vm0/80109080-733561537139611-6782292533398470656-n.jpg',
+      categoryIcon: 'https://i.ibb.co/jR5Sb6N/image-3.png',
+    },
+    {
+      title: 'Give African children a Good Education',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec vitae bibendum dolor, at finibus',
+      imgSrc: 'https://i.ibb.co/Srxxn5f/88310451-791815414647556-1956604263299809280-n.jpg',
+      categoryIcon: 'https://i.ibb.co/jR5Sb6N/image-3.png',
+    },
+    {
+      title: 'Give African children a Good Education',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec vitae bibendum dolor, at finibus ',
+      imgSrc: 'https://i.ibb.co/N638K9K/132891950-1005721596590269-2279651605150191597-n.jpg',
+      categoryIcon: 'https://i.ibb.co/jR5Sb6N/image-3.png',
+    },
+  ];
   return (
     <>
-      <Review />
+      <LatestCampaigns campaigns={campaigns} />
       <Router>
         <Nav />
         <Routes>
