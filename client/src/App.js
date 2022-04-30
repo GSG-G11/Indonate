d3dd502a2233f49f5b21281cecd5c598beb7386b
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-04-30 19:15:50 +0300

    remove App.css Relates #53

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   Nav, SignUp, Signin, Review, OurMission,
