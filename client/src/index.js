b2660d10a44881d87e503e1e0270cb7261ad7310
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-05-05 02:26:44 +0300

    Update react index to use createRoot instead of ReactDom

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/app/store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
 );
