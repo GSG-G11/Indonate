49fc1190087f348e079507c435445bb44cf64458
HaniOlwan <heenoow@gmail.com>
2022-05-05 19:50:08 +0300

    remove static data in app.js Relates #63

--- client/src/App.js
+++ client/src/App.js
@@ -10,31 +10,6 @@ function App() {
   useEffect(() => {
     store.dispatch(getUserData());
   }, []);
-
-  const campaigns = [
-    {
-      id: 0,
-      title: 'Give African children a Good Education',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec vitae bibendum dolor, at finibus ',
-      imgSrc: 'https://i.ibb.co/41n9Vm0/80109080-733561537139611-6782292533398470656-n.jpg',
-      categoryIcon: 'https://i.ibb.co/jR5Sb6N/image-3.png',
-    },
-    {
-      id: 1,
-      title: 'Give African children a Good Education',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec vitae bibendum dolor, at finibus',
-      imgSrc: 'https://i.ibb.co/Srxxn5f/88310451-791815414647556-1956604263299809280-n.jpg',
-      categoryIcon: 'https://i.ibb.co/jR5Sb6N/image-3.png',
-    },
-    {
-      id: 2,
-      title: 'Give African children a Good Education',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec vitae bibendum dolor, at finibus ',
-      imgSrc: 'https://i.ibb.co/N638K9K/132891950-1005721596590269-2279651605150191597-n.jpg',
-      categoryIcon: 'https://i.ibb.co/jR5Sb6N/image-3.png',
-    },
-  ];
-
   return (
     <Router>
       <Nav />
@@ -42,7 +17,7 @@ function App() {
         <Route
           path="/"
           element={
-            <LatestCampaigns campaigns={campaigns} />
+            <LatestCampaigns />
           }
         />
         <Route
