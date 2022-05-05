98d129b519ffb4da1667e69f9ab6abc48b7f6e0f
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-05-05 17:44:28 +0300

    remove params id

--- client/src/components/donateForm/DonateButton.jsx
+++ client/src/components/donateForm/DonateButton.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import Proptypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
 import DonationForm from './DonationForm';
 
 function DonationButton({ campaignId }) {
   const [visible, setVisible] = useState(false);
   const navigate = useNavigate();
-  const { id } = useParams();
   const user = useSelector((state) => state.user);
   const visibleToggle = () => {
     setVisible((prev) => !prev);
@@ -28,7 +27,7 @@ function DonationButton({ campaignId }) {
       <DonationForm
         visible={visible}
         onCancel={visibleToggle}
-        campaignId={id || campaignId}
+        campaignId={campaignId}
         setVisible={setVisible}
       />
     </>
