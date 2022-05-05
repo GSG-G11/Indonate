698edc52e9d45540348aa5ea3b69b984bcaeea04
HaniOlwan <heenoow@gmail.com>
2022-05-06 01:30:54 +0300

    use antd icons Relates #64

--- client/src/components/common/footer/index.jsx
+++ client/src/components/common/footer/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.less';
-import facebook from './facebook1.svg';
-import instagram from './instagram.svg';
-import youtube from './youtube.svg';
-import twitter from './twitter.svg';
-import google from './google.svg';
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  GoogleOutlined,
+  InstagramOutlined,
+  YoutubeOutlined,
+} from '@ant-design/icons';
 import indonate from './indonate.svg';
 
 function Footer() {
@@ -12,15 +14,16 @@ function Footer() {
     <section className="footer_container">
       <div className="hr_lines">
         <hr className="line" />
-        <img className="icon" src={facebook} alt="Facebook" />
-        <img className="icon" src={instagram} alt="Instagram" />
-        <img className="icon" src={youtube} alt="Youtube" />
-        <img className="icon" src={twitter} alt="Twitter" />
-        <img className="icon" src={google} alt="Google" />
+        <FacebookOutlined />
+        <InstagramOutlined />
+        <YoutubeOutlined />
+        <TwitterOutlined />
+        <GoogleOutlined />
         <hr className="line" />
       </div>
       <div>
         <img className="logo" src={indonate} alt="indonate" />
+
       </div>
     </section>
   );
