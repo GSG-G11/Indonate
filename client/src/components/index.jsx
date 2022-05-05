d9158030eeb567de751b9f5ca9e1bc42db08dd87
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-05-05 15:19:30 +0300

    Remove Login and Signup from components

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,7 +1,5 @@
 import OurMission from './OurMission';
 import Nav from './common/Nav';
-import Login from './Login';
-import SignUp from './SignUp';
 import Review from './Review';
 import FilterCampaigns from './FilterCampaigns';
 import DonationButton from './DonateForm/DonateButton';
@@ -10,8 +8,6 @@ import ReportsForm from './ReportsForm';
 export {
   FilterCampaigns,
   Nav,
-  Login,
-  SignUp,
   OurMission,
   Review,
   DonationButton,
