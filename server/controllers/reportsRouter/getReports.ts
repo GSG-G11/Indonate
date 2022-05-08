d9946c9770e065349042e8633f1bda83a5d2106b
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-05-08 11:53:56 +0300

    craete getReports controller Relates #97

---
+++ server/controllers/reportsRouter/getReports.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { Contact } from '../../database/models';
+
+const getReports = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const reports = await Contact.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+    res.json({ message: 'Success', data: reports });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default getReports;
