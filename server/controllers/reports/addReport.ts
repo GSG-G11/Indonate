763186c4da807d1f62211522803e9a432015dddf
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-05-09 22:43:32 +0300

    Rename Contact table with Report

--- server/controllers/reports/addReport.ts
+++ server/controllers/reports/addReport.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { CustomError, reportsSchema } from '../../utils';
-import { Contact } from '../../database/models';
+import { Report } from '../../database/models';
 
 const reports = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await reportsSchema.validateAsync(req.body);
-    await Contact.create(result);
+    await Report.create(result);
     res.status(201).json({ message: 'Report sent successfully' });
   } catch (error) {
     if (error.name === 'ValidationError') next(new CustomError(error.details[0].message, 400));
--- server/controllers/reports/getReports.ts
+++ server/controllers/reports/getReports.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { Contact } from '../../database/models';
+import { Report } from '../../database/models';
 
 const getReports = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const reports = await Contact.findAll({
+    const reports = await Report.findAll({
       attributes: {
         exclude: ['createdAt', 'updatedAt'],
       },
