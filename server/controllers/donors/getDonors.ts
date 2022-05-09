9fc66e335afaefcf59f75dacc5a2e930c930eaf1
farah2003 <farah.shaqoura@example.com>
2022-05-09 12:00:08 +0300

    get donors

--- server/controllers/donors/getDonors.ts
+++ server/controllers/donors/getDonors.ts
@@ -1,11 +1,30 @@
 import { NextFunction, Request, Response } from 'express';
+import { Campaign, Donor } from '../../database/models';
 import { CustomError } from '../../utils';
 import { donorsQuerySchema } from '../../utils/validation';
 
 const getDonors = async (req:Request, res:Response, next:NextFunction) => {
   try {
     await donorsQuerySchema.validateAsync(req.query);
-    res.json({});
+    const { rows: donors, count } = await Donor.findAndCountAll({
+
+      include: {
+        model: Campaign,
+        attributes: ['id', 'title'],
+        through: {
+          attributes: ['food', 'clothes', 'money'],
+        },
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'profile_img', 'password', 'is_admin'],
+      },
+      order: [
+        ['id', 'DESC'],
+      ],
+
+    });
+
+    res.json({ message: 'Success', data: { donors, count } });
   } catch (e) {
     if (e.name === 'ValidationError') {
       next(new CustomError(e.message, 400));
