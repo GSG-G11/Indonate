cbf5ea53762935c3bd864887b06514bf8cd38aa4
HaniOlwan <heenoow@gmail.com>
2022-05-09 23:47:17 +0300

    get all families wrap with try catch Relates #98

--- server/controllers/families/getAllFamilies.ts
+++ server/controllers/families/getAllFamilies.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { Family } from '../../database/models';
 
-const getAllFamilies = async (req:Request, res:Response, next:NextFunction) => {
-  const families = await Family.findAll();
-  res.send(families);
-  next();
+const getAllFamilies = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const families = await Family.findAll();
+    res.json({ message: 'Success', data: { families } });
+  } catch (e) {
+    next(e);
+  }
 };
 
 export default getAllFamilies;
