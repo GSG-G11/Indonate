09bc0053ec9fa8de62aeeaa83f393dbe0910aee4
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-05-10 00:27:58 +0300

    Validate id param and throw an error when family doesn't exists

--- server/controllers/families/deleteFamilyById.ts
+++ server/controllers/families/deleteFamilyById.ts
@@ -1,28 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import { Family } from '../../database/models';
+import { CustomError, paramsSchema } from '../../utils';
 
 const deleteFamilyById = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const { id } = req.params;
   try {
+    const { id } = req.params;
+    await paramsSchema.validateAsync(req.params);
     const deletedFamily = await Family.destroy({
       where: {
         id,
       },
     });
     if (!deletedFamily) {
-      res.status(400).json({
-        message: "The family you are trying to delete doesn't exist",
-      });
+      throw new CustomError(
+        "The family you are trying to delete doesn't exist",
+        400,
+      );
     } else {
       res.json({
         message: 'Family deleted successfully',
       });
     }
   } catch (error) {
+    if (error.name === 'ValidationError') {
+      next(new CustomError(error.message, 400));
+    }
     next(error);
   }
 };
