db53793d4a8f2b7bf2b1236a3427c544b5dc9538
farah2003 <farah.shaqoura@example.com>
2022-05-11 22:20:41 +0300

    remove donor files

--- server/routes/donors.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { getDonors } from '../controllers';
-
-const donorsRoute = Router();
-donorsRoute.get('/admin/donors', getDonors);
-export default donorsRoute;
