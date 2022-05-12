977c6f27bb73375b6bce91b1b229e6f4fb3cbd60
farah2003 <farah.shaqoura@example.com>
2022-05-12 22:27:58 +0300

    update campaign

--- server/controllers/admin/campaign/postCampaignFamilies.ts
+++ server/controllers/admin/campaign/postCampaignFamilies.ts
@@ -1,12 +1,20 @@
-import { Request, Response } from 'express';
-import { paramsSchema } from '../../../utils';
+import { NextFunction, Request, Response } from 'express';
+import { Campaign } from '../../../database/models';
+import { CustomError, paramsSchema } from '../../../utils';
 
-const postCampaignFamilies = async (req:Request, res:Response) => {
+const postCampaignFamilies = async (req:Request, res:Response, next:NextFunction) => {
   try {
-    const { id } = req.params;
+    const { params: { id: campaignId } } = req;
     await paramsSchema.validateAsync(req.params);
+    const result = await Campaign.update({ is_available: false }, { where: { campaignId } });
+    if (!result[0]) throw new CustomError('Update campaigns failed', 400);
+    res.json(result);
   } catch (error) {
+    if (error.name === 'ValidationError') {
+      next(new CustomError(error.message, 400));
+    }
     console.log(error);
+    next(error);
   }
 };
 export default postCampaignFamilies;
