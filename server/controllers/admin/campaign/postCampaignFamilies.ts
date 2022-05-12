29d83039dcb12a76a85b2cfdd463446f133d3430
farah2003 <farah.shaqoura@example.com>
2022-05-13 01:44:23 +0300

    check if the families id exit and make validation for req body

--- server/controllers/admin/campaign/postCampaignFamilies.ts
+++ server/controllers/admin/campaign/postCampaignFamilies.ts
@@ -1,27 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
-import { Campaign, Capon } from '../../../database/models';
+import { Campaign, Capon, Family } from '../../../database/models';
 import { CustomError, paramsSchema } from '../../../utils';
+import { familiesForCampaignSchema } from '../../../utils/validation';
 
 const postCampaignFamilies = async (req:Request, res:Response, next:NextFunction) => {
   try {
     const { params: { id: campaignId } } = req;
     const {
       body: {
-        id: familiesId,
+        ids: familiesIds,
         food,
         clothes,
         money,
       },
     } = req;
     await paramsSchema.validateAsync(req.params);
-    const result = await Campaign.update({ is_available: false }, { where: { id: campaignId } });
-    if (!result[0]) throw new CustomError('Update campaigns failed', 400);
-    await Promise.all(JSON.parse(familiesId).map(async (familyId:any) => {
+    const updateCampaignResponse = await Campaign.update(
+      { is_available: false },
+      { where: { id: campaignId } },
+    );
+
+    if (!updateCampaignResponse[0]) throw new CustomError('Update campaign failed', 400);
+
+    await familiesForCampaignSchema.validateAsync({
+      ids: JSON.parse(familiesIds), food, clothes, money,
+    });
+
+    await Promise.all(JSON.parse(familiesIds).map(async (familyId:any) => {
+      const family = await Family.findByPk(familyId, {
+        raw: true,
+      });
+      if (!family) throw new CustomError('Cannot add families', 400);
+    }));
+
+    await Promise.all(JSON.parse(familiesIds).map(async (familyId:any) => {
       await Capon.create({
         campaignId, familyId, food, clothes, money,
       });
     }));
-    res.json({ message: 'Success' });
+
+    res.json({ message: 'Families added successfully' });
   } catch (error) {
     if (error.name === 'ValidationError') {
       next(new CustomError(error.message, 400));
--- server/utils/validation/index.ts
+++ server/utils/validation/index.ts
@@ -5,6 +5,7 @@ import paramsSchema from './paramsSchema';
 import reportsSchema from './reportsSchema';
 import donationSchema from './donationSchema';
 import familySchema from './familySchema';
+import familiesForCampaignSchema from './familiesForCampaignSchema';
 
 export {
   loginSchema,
@@ -14,4 +15,5 @@ export {
   donationSchema,
   querySchema,
   familySchema,
+  familiesForCampaignSchema,
 };
