e9eb782fb5b6d1233ca6fcdb9602dd297ac130c6
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-05-15 00:10:23 +0300

    Change columns structure and add pagination

--- client/src/pages/admin/CampaignsTable/index.jsx
+++ client/src/pages/admin/CampaignsTable/index.jsx
@@ -1,25 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import {
-  Button,
   Collapse,
-  Image,
-  Skeleton,
   Table,
   Popconfirm,
   Popover,
   Space,
+  message,
+  Pagination,
 } from 'antd';
-import { CloseCircleOutlined, DeleteOutlined } from '@ant-design/icons';
+import {
+  CloseCircleOutlined,
+  DeleteOutlined,
+  FileSearchOutlined,
+} from '@ant-design/icons';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './style.css';
 
-const { Panel } = Collapse;
-const { Column } = Table;
-
 function CampaignsTable() {
   const [campaigns, setCampaigns] = useState([]);
+  const [page, setPage] = useState(1);
+  const [campaignsCount, setCampaignsCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
@@ -29,18 +31,21 @@ function CampaignsTable() {
       try {
         const {
           data: {
-            data: { campaigns: campaignsData },
+            data: { campaigns: campaignsData, count },
           },
-        } = await axios.get('/api/admin/campaigns', {
+        } = await axios.get(`/api/admin/campaigns?page=${page}`, {
           cancelToken: source.token,
         });
         setCampaigns(campaignsData);
+        setCampaignsCount(count);
         setIsLoading(false);
+        setIsError(false);
       } catch ({
         response: {
           data: { message: errorMessage },
         },
       }) {
+        message.error(errorMessage);
         setIsError(true);
       }
     };
@@ -48,108 +53,193 @@ function CampaignsTable() {
     return () => {
       source.cancel();
     };
-  }, []);
+  }, [page]);
   const handleDeleteCampaign = async (id) => {
     setCampaigns(campaigns.filter((campaign) => campaign.id !== id));
-    const deleteCampaign = await axios.delete(`/api/admin/campaigns/1${id}`);
+    try {
+      const {
+        data: { message: deleteMessage },
+      } = await axios.delete(`/api/admin/campaigns/${id}`);
+      message.success(deleteMessage);
+    } catch ({
+      response: {
+        data: { message: errorMessage },
+      },
+    }) {
+      message.error(errorMessage);
+    }
   };
+  const handleCloseCampaign = async (id) => {
+    try {
+      const {
+        data: { message: deleteMessage },
+      } = await axios.patch(`/api/admin/campaign/${id}`);
+      message.success(deleteMessage);
+    } catch ({
+      response: {
+        data: { message: errorMessage },
+      },
+    }) {
+      message.error(errorMessage);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+      render: (title, record) => (
+        <Link
+          to={`/campaign/${record.id}`}
+          className="campaign-table-campaign-title"
+        >
+          {title.split(' ').slice(0, 5).join(' ')}
+        </Link>
+      ),
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      align: 'center',
+      key: 'description',
+      render: (description) => (
+        <Popover placement="bottom" content={description} trigger="click">
+          <FileSearchOutlined style={{ fontSize: '2.5rem' }} />
+        </Popover>
+      ),
+    },
+    {
+      title: 'Target',
+      children: [
+        {
+          title: 'Money',
+          align: 'center',
+          dataIndex: 'money_target',
+          key: 'money_target',
+          render: (value) => (
+            <>
+              {value}
+              $
+            </>
+          ),
+        },
+        {
+          title: 'Food',
+          dataIndex: 'food_target',
+          align: 'center',
+          key: 'food_target',
+          render: (value) => (
+            <>
+              {value}
+              {' '}
+              Meal
+            </>
+          ),
+        },
+        {
+          title: 'Clothes',
+          align: 'center',
+          dataIndex: 'clothes_target',
+          key: 'clothes_target',
+          render: (value) => (
+            <>
+              {value}
+              {' '}
+              Piece
+            </>
+          ),
+        },
+      ],
+    },
+    {
+      title: 'Current',
+      children: [
+        {
+          title: 'Money',
+          dataIndex: 'current_money',
+          align: 'center',
+          key: 'current_money',
+          render: (value) => (
+            <>
+              {value}
+              $
+            </>
+          ),
+        },
+        {
+          title: 'Food',
+          align: 'center',
+          dataIndex: 'current_food',
+          key: 'current_food',
+          render: (value) => (
+            <>
+              {value}
+              {' '}
+              Meal
+            </>
+          ),
+        },
+        {
+          title: 'Clothes',
+          dataIndex: 'current_clothes',
+          align: 'center',
+
+          key: 'current_clothes',
+          render: (value) => (
+            <>
+              {value}
+              {' '}
+              Piece
+            </>
+          ),
+        },
+      ],
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      align: 'center',
+      render: (_, record) => (
+        <Space>
+          <Popconfirm
+            title="Are you sure？"
+            onConfirm={() => {
+              handleDeleteCampaign(record.id);
+            }}
+            okText="Yes"
+            cancelText="No"
+            okType="danger"
+          >
+            <DeleteOutlined style={{ fontSize: '2.5rem', color: 'red' }} />
+          </Popconfirm>
+          <Popconfirm
+            title="Sure to close?"
+            onConfirm={() => {
+              handleCloseCampaign(record.id);
+            }}
+            okText="Yes"
+            cancelText="No"
+            okType="primary"
+          >
+            <CloseCircleOutlined style={{ fontSize: '2.5rem' }} />
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
   return !isError ? (
-    <div className="campaigns-table-container">
-      {!isLoading ? (
-        <Table dataSource={campaigns}>
-          <Column
-            title="Image"
-            dataIndex="image_link"
-            key="image_link"
-            render={(link) => (
-              <Image
-                id="campaign-table-campaign-image"
-                src={link}
-                width={100}
-                height={50}
-              />
-            )}
-          />
-          <Column
-            title="Title"
-            dataIndex="title"
-            key="title"
-            render={(title, record) => (
-              <Link
-                to={`/campaign/${record.id}`}
-                className="campaign-table-campaign-title"
-              >
-                {title.split(' ').slice(0, 5).join(' ')}
-              </Link>
-            )}
-          />
-          <Column
-            title="Description"
-            dataIndex="description"
-            key="description"
-            render={(description) => (
-              <>
-                {description.split(' ').slice(0, 6).join(' ')}
-                {' '}
-                ...
-                <Popover content={description} trigger="click">
-                  <Button type="link">Expand</Button>
-                </Popover>
-              </>
-            )}
-          />
-          <Column
-            title="Target Money"
-            dataIndex="money_target"
-            key="money_target"
-          />
-          <Column
-            title="Target Food"
-            dataIndex="food_target"
-            key="food_target"
-          />
-          <Column
-            title="Target Clothes"
-            dataIndex="clothes_target"
-            key="clothes_target"
-          />
-          <Column
-            title="Actions"
-            dataIndex=""
-            key="x"
-            render={(_, record) => (
-              <Space>
-                <Popconfirm
-                  title="Are you sure？"
-                  onConfirm={() => {
-                    handleDeleteCampaign(record.id);
-                  }}
-                  okText="Yes"
-                  cancelText="No"
-                  okType="danger"
-                >
-                  <DeleteOutlined style={{ fontSize: '2rem', color: 'red' }} />
-                </Popconfirm>
-                <Popconfirm
-                  title="Are you sure？"
-                  onConfirm={() => {
-                    handleDeleteCampaign(record.id);
-                  }}
-                  okText="Yes"
-                  cancelText="No"
-                  okType="danger"
-                >
-                  <CloseCircleOutlined style={{ fontSize: '2rem' }} />
-                </Popconfirm>
-              </Space>
-            )}
-          />
-          <Column title="id" dataIndex="id" key="id" />
-        </Table>
-      ) : (
-        <Skeleton active />
-      )}
-    </div>
+    <Table
+      dataSource={campaigns}
+      bordered
+      loading={isLoading}
+      columns={columns}
+      pagination={{ total: campaignsCount, defaultPageSize: 8 }}
+      onChange={(e) => {
+        setPage(e.current);
+      }}
+    />
   ) : (
     <div>{isError}</div>
   );
