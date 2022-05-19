f5c58b4d7cea10e937445db123713582a2de8640
HaniOlwan <heenoow@gmail.com>
2022-05-19 15:42:05 +0300

    check if donor has donation throw error before deletion

--- server/controllers/admin/donors/deleteDonorById.ts
+++ server/controllers/admin/donors/deleteDonorById.ts
@@ -1,14 +1,20 @@
 import { NextFunction, Response, Request } from 'express';
-import { Donor } from '../../../database/models';
+import { Donor, Donation } from '../../../database/models';
 import { CustomError, paramsSchema } from '../../../utils';
 
 const deleteDonorById = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = await paramsSchema.validateAsync(req.params);
+    const donationsForDonor = await Donation.findAll({
+      where: {
+        donorId: id,
+      },
+    });
+    if (donationsForDonor.length) throw new CustomError('You cannot delete this donor', 400);
     const deletedDonor = await Donor.destroy({
       where: { id },
     });
-    if (!deletedDonor) throw new CustomError('The donor you are trying to delete does not exist', 400);
+    if (!deletedDonor) { throw new CustomError('The donor you are trying to delete does not exist', 400); }
     res.json({ message: 'Donor deleted successfully' });
   } catch (error) {
     if (error.name === 'ValidationError') next(new CustomError(error.details[0].message, 400));
